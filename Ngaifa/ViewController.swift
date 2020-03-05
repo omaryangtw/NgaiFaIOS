47b351ffbd7898d7ad76475692165a5018bd7dd8
omaryangtw <omaryang.tw@gmail.com>
2020-03-05 19:22:18 +0800

    Initial Commit

---
+++ Ngaifa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ngaifa
+//
+//  Created by Yang Omar on 2020/3/5.
+//  Copyright © 2020 Yang Omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
